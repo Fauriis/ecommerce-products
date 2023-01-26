58fc961a488229a5cc21a830b336c9412e87775c
Fauriis <dobreflorentina58@gmail.com>
2023-01-26 17:15:00 +0200

    app

---
+++ ecom/pages/contact.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const ContactPage = () => {
+    return <>
+    <Head>
+        <title>Contact PAGE</title>
+
+        <main> hello asfd</main>
+    </Head>
+    </>
+}
+
+export default ContactPage
