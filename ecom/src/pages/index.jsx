404adbcfb35d228431aefe474faf24cedb2f18f0
Fauriis <dobreflorentina58@gmail.com>
2023-03-02 17:51:55 +0200

    app

--- ecom/src/pages/index.jsx
+++ ecom/src/pages/index.jsx
@@ -1,32 +1,37 @@
 import Head from "next/head";
 
 import { Layout } from "../layouts";
+import { motion } from "framer-motion";
 
 const Home = () => {
-
-
   return (
-   <>
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
 
-   <Head>
-    <title>Homepage</title>
-   </Head>
+      <Layout>
+        <main className="bg-red-200">
+          <div>
+            <motion.img
+              initial={{
+                x: -60,
+                opacity: 0,
+              }}
+              whileInView={{ x: 0, opacity: 1 }}
+              src="banner.png"
+              alt="new collection"
+              className="w-full"
+            ></motion.img>
+          </div>
+        </main>
 
-   <Layout>
-    <main className="bg-red-200">
-     <div>
-      <h1>See the latest collection!</h1>
-
-      <h3>A brand to remember!</h3>
-     </div>
-    </main>
-
-    <section className="bg-zinc-100">
-<h1>more to come</h1>
-<p>Are you ready?</p>
-    </section>
-   </Layout>
-   </>
+        <section className="bg-zinc-100">
+          <h1>more to come</h1>
+          <p>Are you ready?</p>
+        </section>
+      </Layout>
+    </>
   );
 };
 
