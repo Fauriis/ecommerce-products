eb3b1940333a1781fb6f21a47eb0b8d145de5a7a
Fauriis <dobreflorentina58@gmail.com>
2023-03-29 13:29:06 +0300

    app

--- ecom/src/components/catalog/ProductGrid.jsx
+++ ecom/src/components/catalog/ProductGrid.jsx
@@ -1,8 +1,8 @@
 import { css } from "@emotion/css";
 import React from "react";
 import ProductsNotFound from "../../pages/noproducts";
-import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 export const ProductGrid = ({ products = [], perRow = 4 }) => {
   if (products.length <= 0) {
@@ -22,31 +22,34 @@ export const ProductGrid = ({ products = [], perRow = 4 }) => {
   return (
     <ul className={gridCss}>
       {products.map((product, index) => {
-        const { title, price, image } = product;
-        const formattedPrice = new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD',
-        }).format(price)
+        const { title, price, image, id } = product;
+        const formattedPrice = new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(price);
 
         return (
           <li key={index}>
             <article className="w-full">
               <header>
                 <div className="w-full h-72 text-center">
-                  <motion.img
-                    initial={{
-                      x: -60,
-                      opacity: 0,
-                    }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{
-                      scale: 0.8,
-                      borderRadius: "10%",
-                    }}
-                    src={image}
-                    className="h-full inline cursor-pointer"
-                  ></motion.img>
+                  <Link href={`/products/${id}`}>
+                    <motion.img
+                      initial={{
+                        x: -60,
+                        opacity: 0,
+                      }}
+                      whileInView={{ x: 0, opacity: 1 }}
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{
+                        scale: 0.8,
+                        borderRadius: "10%",
+                      }}
+                      src={image}
+                      title={title}
+                      className="h-full inline cursor-pointer"
+                    ></motion.img>
+                  </Link>
                 </div>
               </header>
 
@@ -55,7 +58,9 @@ export const ProductGrid = ({ products = [], perRow = 4 }) => {
                   {title}
                 </h1>
 
-                <div className="text-zinc-500 font-light mb-2">{formattedPrice}</div>
+                <div className="text-zinc-500 font-light mb-2">
+                  {formattedPrice}
+                </div>
               </section>
             </article>
           </li>
--- ecom/src/pages/products/[pid].jsx
+++ ecom/src/pages/products/[pid].jsx
@@ -1,17 +1,51 @@
 import Head from "next/head";
 import { Layout } from "../../layouts";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { baseUrl } from "../..";
+import { CartControl } from "../../components/cart/CartControl";
 
-const ProductPage = ({ product }) => {
+const ProductPage = () => {
+  const router = useRouter();
+  const { pid } = router.query;
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    if (pid === undefined) {
+      return;
+    }
+
+    fetch(`${baseUrl}/products/${pid}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        setProduct(result);
+      });
+  }, [pid]);
+
+  if (product === null) {
+    return <></>;
+  }
+
+  const { id, title, description, price, image } = product;
+
   return (
     <>
       <Head>
-        <title>Product Page</title>
+        <title>{title}</title>
       </Head>
 
       <Layout>
-        <main>
-            product
-        </main>
+        <header className="container px-4 lg:px-0 mx-auto flex justify-between">
+          <div>Back to shop</div>
+
+          <CartControl></CartControl>
+        </header>
+
+        <section className="container px-4 lg:px-0 mx-auto">s</section>
+        <section className="border-t"></section>
+        <section className="container px-4 lg:px-0 mx-auto">v</section>
       </Layout>
     </>
   );
